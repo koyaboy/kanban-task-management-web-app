8ecd8ab9480c007b51c19d4267f2616a6c77d3db
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-01 14:25:52 +0100

    pass data from view-task to edit-task

--- src/app/components/edit/edit-task/edit-task.component.ts
+++ src/app/components/edit/edit-task/edit-task.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Subtask } from '../../../model/subtask';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-edit-task',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './edit-task.component.html',
   styleUrl: './edit-task.component.css'
 })
 export class EditTaskComponent {
-
+  @Input() title!: string
+  @Input() description!: string
+  @Input() subtasks!: Array<Subtask>
+  @Input() status!: string
 }
