b349f4eb1cc167243d9c8b28b5f8293cdd428b53
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-01 14:09:16 +0100

    implement edit and delete task popup in view-task

--- src/app/components/view-task/view-task.component.ts
+++ src/app/components/view-task/view-task.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core';
 import { Subtask } from '../../model/subtask';
 import { NgFor, NgStyle } from '@angular/common';
+import { ViewContainerRef } from '@angular/core';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-view-task',
   standalone: true,
-  imports: [NgFor, NgStyle],
+  imports: [NgFor, NgStyle, OverlayModule],
   templateUrl: './view-task.component.html',
-  styleUrl: './view-task.component.css'
+  styleUrl: './view-task.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ViewTaskComponent {
+  viewContainerRef: ViewContainerRef = inject(ViewContainerRef)
+
   @Input() title!: string
   @Input() description!: string
   @Input() subtasks!: Array<Subtask>
   @Input() status!: string
 
   completedSubtasks!: number
+  openEditandDeleteOptions = false
 
   ngOnInit() {
     this.completedSubtasks = this.subtasks.filter((task) => task.isCompleted).length
