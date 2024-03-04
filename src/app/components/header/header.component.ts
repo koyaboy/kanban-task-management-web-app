43799d0031fbdbcd2154079c26df27affdceb280
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-04 15:17:16 +0100

    implement add-board modal design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,13 +3,14 @@ import { BoardService } from '../../services/board.service';
 import { Board } from '../../model/board';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { AddTaskComponent } from '../add/add-task/add-task.component';
+import { AddBoardComponent } from '../add/add-board/add-board.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AddTaskComponent, OverlayModule, NgFor, NgStyle],
+  imports: [AddTaskComponent, AddBoardComponent, OverlayModule, NgFor, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -24,10 +25,18 @@ export class HeaderComponent {
   shouldOpenBoards: boolean = false
 
   @ViewChild("addTaskRef") addTaskRef!: TemplateRef<any>
+  @ViewChild("addBoardRef") addBoardRef!: TemplateRef<any>
 
-  openAddBoardModal() {
+  openAddTaskModal() {
     const addTaskPortal = new TemplatePortal(this.addTaskRef, this.viewContainerRef)
     this.boardService.openModal(addTaskPortal)
 
+  }
+
+  openAddBoardModal(): void {
+    this.shouldOpenBoards = false
+
+    const addBoardPortal = new TemplatePortal(this.addBoardRef, this.viewContainerRef)
+    this.boardService.openModal(addBoardPortal)
   }
 }
