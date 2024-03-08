df24d00e8368e30ae2f98ea1cc37d8618940c3be
Koya <44787464+koyaboy@users.noreply.github.com>
2024-03-08 11:20:24 +0100

    display columns in view-task status select dropdown

--- src/app/components/view-task/view-task.component.ts
+++ src/app/components/view-task/view-task.component.ts
@@ -7,6 +7,7 @@ import { EditTaskComponent } from '../edit/edit-task/edit-task.component';
 import { TemplatePortal } from '@angular/cdk/portal';
 import { BoardService } from '../../services/board.service';
 import { DeleteTaskComponent } from '../delete/delete-task/delete-task.component';
+import { Board } from '../../model/board';
 
 @Component({
   selector: 'app-view-task',
@@ -20,6 +21,7 @@ export class ViewTaskComponent {
   boardService: BoardService = inject(BoardService)
   viewContainerRef: ViewContainerRef = inject(ViewContainerRef)
 
+  @Input() board !: Board | undefined
   @Input() title!: string
   @Input() description!: string
   @Input() subtasks!: Array<Subtask>
