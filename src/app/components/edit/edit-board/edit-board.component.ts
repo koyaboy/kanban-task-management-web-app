6aa32dd60f9917ea3f2b73c36089b30c3d8b1c9b
Koya <44787464+koyaboy@users.noreply.github.com>
2024-04-10 07:32:32 +0100

    implement design of edit-board form

--- src/app/components/edit/edit-board/edit-board.component.ts
+++ src/app/components/edit/edit-board/edit-board.component.ts
@@ -1,14 +1,68 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Board } from '../../../model/board';
 import { NgFor } from '@angular/common';
+import { BoardService } from '../../../services/board.service';
+import { FormBuilder, FormGroup, Validators, FormArray, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Column } from '../../../model/column';
 
 @Component({
   selector: 'app-edit-board',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, ReactiveFormsModule, FormsModule],
   templateUrl: './edit-board.component.html',
   styleUrl: './edit-board.component.css'
 })
 export class EditBoardComponent {
-  @Input() board!: Board | undefined
+  boardService: BoardService = inject(BoardService)
+  fb: FormBuilder = inject(FormBuilder)
+
+  // @Output() updatedBoards: EventEmitter<Board[]> = new EventEmitter()
+  selectedBoardName!: string
+  selectedBoardColumns!: Column[] | undefined
+  editBoardForm!: FormGroup
+
+  ngOnInit() {
+    this.boardService.selectedBoard$.subscribe((selectedBoard) => {
+      this.selectedBoardName = selectedBoard.name
+      this.selectedBoardColumns = selectedBoard.columns
+    })
+
+    this.editBoardForm = this.fb.group({
+      boardName: [this.selectedBoardName, Validators.required],
+      boardColumns: this.fb.array((this.selectedBoardColumns || []).map(column => this.fb.group({ name: [column.name, Validators.required] })))
+    })
+  }
+
+  get boardColumns() {
+    return this.editBoardForm.get('boardColumns') as FormArray;
+  }
+
+  addBoardColumn(): void {
+    this.boardColumns.push(this.fb.group({ name: ['', Validators.required] }))
+  }
+
+  removeBoardColumn(index: number): void {
+    this.boardColumns.removeAt(index)
+  }
+
+  onSubmit(): void {
+    const { boardName, boardColumns } = this.editBoardForm.value
+
+    const data = {
+      name: boardName,
+      columns: boardColumns
+    }
+
+    console.log(data)
+
+    // this.boardService.addBoard(data).subscribe(() => {
+    //   this.updateBoards()
+    // })
+  }
+
+  updateBoards() {
+    // this.boardService.getBoards().subscribe((data) => {
+    //   this.updatedBoards.emit(data)
+    // })
+  }
 }
